47f538f630bdb64e969e5b09ea4c14bb37ecb8c2
Mohamed Ali Haoufa <119907177+MohamedAliHaoufa@users.noreply.github.com>
2023-09-30 16:32:30 +0100

    Delete stm32f4xx_drivers directory

--- stm32f4xx_drivers/Debug/Src/001led_Toggle.d
+++
@@ -1,9 +0,0 @@
-Src/001led_Toggle.o: ../Src/001led_Toggle.c \
- ../Src/../drivers/Inc/stm32f407xx.h \
- ../Src/../drivers/Inc/stm32f407xx_gpio_driver.h \
- ../Src/../drivers/Inc/stm32f407xx.h \
- ../Src/../drivers/Inc/stm32f407xx_i2c_driver.h
-../Src/../drivers/Inc/stm32f407xx.h:
-../Src/../drivers/Inc/stm32f407xx_gpio_driver.h:
-../Src/../drivers/Inc/stm32f407xx.h:
-../Src/../drivers/Inc/stm32f407xx_i2c_driver.h:
